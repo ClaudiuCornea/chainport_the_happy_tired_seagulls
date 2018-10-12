e50fa84171edd10f052725e6457c7138c8626107
julie doms <juliedoms@MacBook-Pro-de-julie-2.local>
2018-10-12 15:37:49 +0200

    template1

---
+++ src/views/Dashboard/Dashboard.jsx
@@ -0,0 +1,140 @@
+import React from "react";
+import {
+  Card,
+  CardHeader,
+  CardBody,
+  CardFooter,
+  CardTitle,
+  Row,
+  Col,
+  UncontrolledDropdown,
+  DropdownToggle,
+  DropdownMenu,
+  DropdownItem,
+  Table
+} from "reactstrap";
+// react plugin used to create charts
+import { Line, Bar } from "react-chartjs-2";
+// function that returns a color based on an interval of numbers
+
+import { PanelHeader, Stats, CardCategory, Tasks } from "components";
+
+import {
+  dashboardPanelChart,
+  dashboardShippedProductsChart,
+  dashboardAllProductsChart,
+  dashboard24HoursPerformanceChart
+} from "variables/charts.jsx";
+
+import { tasks } from "variables/general.jsx";
+
+class Dashboard extends React.Component {
+  render() {
+    return (
+      <div>
+        <PanelHeader
+          size="lg"
+          content={
+            <Line
+              data={dashboardPanelChart.data}
+              options={dashboardPanelChart.options}
+            />
+          }
+        />
+        
+        
+        
+        <div className="content">
+            
+            <Col xs={12} md={12}>
+              <Card className="card-chart">
+                <CardHeader>
+                  <CardCategory>real time</CardCategory>
+                  <CardTitle tag="h4">Solar energy</CardTitle>
+                  <UncontrolledDropdown>
+                    <DropdownToggle
+                      className="btn-round btn-simple btn-icon"
+                      color="default"
+                    >
+                      <i className="now-ui-icons loader_gear" />
+                    </DropdownToggle>
+                    <DropdownMenu right>
+                      <DropdownItem>Action</DropdownItem>
+                      <DropdownItem>Another Action</DropdownItem>
+                      <DropdownItem>Something else here</DropdownItem>
+                      <DropdownItem className="text-danger">
+                        Remove data
+                      </DropdownItem>
+                    </DropdownMenu>
+                  </UncontrolledDropdown>
+                </CardHeader>
+                <CardBody>
+                  <div className="chart-area">
+                    <Line
+                      data={dashboardAllProductsChart.data}
+                      options={dashboardAllProductsChart.options}
+                    />
+                  </div>
+                </CardBody>
+                <CardFooter>
+                  <Stats>
+                    {[
+                      {
+                        i: "now-ui-icons arrows-1_refresh-69",
+                        t: "Just Updated"
+                      }
+                    ]}
+                  </Stats>
+                </CardFooter>
+              </Card>
+            </Col>
+            
+            <Col xs={12} md={12}>
+              <Card className="card-chart">
+                <CardHeader>
+                  <CardCategory>Real time</CardCategory>
+                  <CardTitle tag="h4">Wind energy</CardTitle>
+                  <UncontrolledDropdown>
+                    <DropdownToggle
+                      className="btn-round btn-simple btn-icon"
+                      color="default"
+                    >
+                      <i className="now-ui-icons loader_gear" />
+                    </DropdownToggle>
+                    <DropdownMenu right>
+                      <DropdownItem>Action</DropdownItem>
+                      <DropdownItem>Another Action</DropdownItem>
+                      <DropdownItem>Something else here</DropdownItem>
+                      <DropdownItem className="text-danger">
+                        Remove data
+                      </DropdownItem>
+                    </DropdownMenu>
+                  </UncontrolledDropdown>
+                </CardHeader>
+                <CardBody>
+                  <div className="chart-area">
+                    <Line
+                      data={dashboardAllProductsChart.data}
+                      options={dashboardAllProductsChart.options}
+                    />
+                  </div>
+                </CardBody>
+                <CardFooter>
+                  <Stats>
+                    {[
+                      {
+                        i: "now-ui-icons arrows-1_refresh-69",
+                        t: "Just Updated"
+                      }
+                    ]}
+                  </Stats>
+                </CardFooter>
+              </Card>
+            </Col>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default Dashboard;
