88ccab47ec2b500db5fb5aa9ba8eccfad3be3338
ClaudiuCornea <claudiu.simion.cornea@gmail.com>
2018-10-12 16:18:56 +0200

    graph_function

--- src/App.js
+++ src/App.js
@@ -1,28 +1,35 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import { LineChart, Line, XAxis, YAxis, CartesianGrid, Tooltip, Legend} from 'recharts';
+
+const data = [
+      {name: 'Page A', uv: 4000, pv: 2400, amt: 2400},
+      {name: 'Page B', uv: 3000, pv: 1398, amt: 2210},
+      {name: 'Page C', uv: 2000, pv: 9800, amt: 2290},
+      {name: 'Page D', uv: 2780, pv: 3908, amt: 2000},
+      {name: 'Page E', uv: 1890, pv: 4800, amt: 2181},
+      {name: 'Page F', uv: 2390, pv: 3800, amt: 2500},
+      {name: 'Page G', uv: 3490, pv: 4300, amt: 2100},
+];
+
+
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
+          <div className='lol'>
+            <LineChart width={800} height={400} data={data}
+            margin={{top: 50, right: 50, left: 50, bottom: 50}}>
+                <Line type="monotone" dataKey="uv" stroke="#8884d8"/>
+                <XAxis dataKey="name"/>
+                <YAxis/>
+                <Tooltip/>
+            </LineChart>
+            </div>
     );
   }
 }
+
 
 export default App;
---
+++ src/view/Lol.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+
+class Lol extends Component {
+    render() {
+        return (
+            <div className="Lol">
+                 <p>
+                    Lol
+                </p>
+            </div>
+        );
+    }
+}
+
+export default Lol;
